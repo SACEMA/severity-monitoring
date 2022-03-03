7eb55cb0fee7f1c194a13210b9daacccaa1e2dda
Bingham <jeremyb@sun.ac.za>
2022-03-03 12:08:19 +0200

    est_sec running; sharing to work separately on understanding outputs.

--- estimate_101.R
+++ estimate_101.R
@@ -6,12 +6,13 @@ library(EpiNow2)
 library(tidyverse)
 
 
-?estimate_secondary
+# ?estimate_secondary
 
 
 dd_flat = readRDS('./data/flat-ts-fixed-proportion.rds')
 
-sec_delays = delay_opts(list(mean = log(14), mean_sd = 0.5, sd =  log(7), sd_sd = 0.25, max = 30))
+sec_delays = delay_opts(list(mean = log(5), mean_sd = 0.1, sd =  log(1.01), sd_sd = 0.00001, max = 10))
 
 
+out_flat <- estimate_secondary(reports = dd_flat,  delays= sec_delays)
 
