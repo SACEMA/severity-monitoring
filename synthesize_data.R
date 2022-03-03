4fb1c9d0fad7334389034063e8f0c76b9c419390
James Azam <jamesazam@sun.ac.za>
2022-03-03 11:51:30 +0200

    Added some comments

--- synthesize_data.R
+++ synthesize_data.R
@@ -1,14 +1,19 @@
+# Set up synthetic data 
+# March 2022
+# Jeremy, James, Juliet, Carl
+
+#Load packages
 library(dplyr)
 library(lubridate)
 
 
-#inputs
+#Inputs
 fixed_delay <- 5 #delay until secondary outcomes become observable
 prop_to_seconday <- 0.1 #fixed proportion of primary outcomes that become secondary outcomes
 flat_primary <- 10 #fixed number of primary outcomes per day
 
 
-
+#Create the fake data
 reported_data <- tibble(date = ymd('2021-01-01') + 0:99,
            primary = rep(10, length(date)),
            secondary = c(rep(NA, times = fixed_delay), 
