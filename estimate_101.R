a1d09d1d8f56cb4138fee7888b9312f3406f909e
Bingham <jeremyb@sun.ac.za>
2022-03-03 12:46:18 +0200

    mid-explore. pushing to keep synchronised

--- estimate_101.R
+++ estimate_101.R
@@ -7,13 +7,48 @@ library(tidyverse)
 
 
 # ?estimate_secondary
-
+# -- setup --
 
 dd_flat = readRDS('./data/flat-ts-fixed-proportion.rds')
 
-sec_delays = delay_opts(list(mean = log(5), mean_sd = 0.1, sd =  log(1.01), sd_sd = 0.00001, max = 10))
+sec_delays = delay_opts(list(mean = log(5),
+                             mean_sd = 0.00001,
+                             sd =  log(1.01),
+                             sd_sd = 0.0000001,
+                             max = 10
+                             ))
+
+obs_no_week = obs_opts(week_effect = FALSE)
+
+sec_type = secondary_opts('incidence')
+
+# run with data + default parameters
+
+out_flat_default <- estimate_secondary(reports = dd_flat)
+
+plot(out_flat_default, primary = TRUE)
 
 
-out_flat <- estimate_secondary(reports = dd_flat,  delays= sec_delays, burn_in = 30, )
+# run with data + no week + no delay parameters
 
-plot(out_flat)
+out_flat_simple <- estimate_secondary(reports = dd_flat,
+                                       obs = obs_no_week,
+                                       delays = delay_opts(),
+                                      secondary = sec_type)
+
+plot(out_flat_simple, primary = TRUE)
+
+out_flat_simple[['predictions']] %>% ggplot(aes(x = date)) +
+  geom_line(aes(y = median)) + 
+  geom_line(aes(y = primary), color = 'blue') +
+  geom_line(aes(y = secondary), color = 'green')
+
+
+# specify delays and week effect = FALSE
+
+out_flat <- estimate_secondary(reports = dd_flat,
+                               delays= sec_delays,
+                               burn_in = 30,
+                               obs = obs_no_week)
+
+plot(out_flat, primary = TRUE)
