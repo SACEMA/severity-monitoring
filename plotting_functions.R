37e70399f15223cbfd3cece926f89c133f196a93
Bingham <jeremyb@sun.ac.za>
2022-03-07 15:08:30 +0200

    plotting function

---
+++ plotting_functions.R
@@ -0,0 +1,16 @@
+# functions for plotting outputs of estimate_secondary
+# March 2022
+# JB, JA, JRCP, CABP
+
+plot_est_sec_out <- function(predictions,
+                             plot_title = "obs_opts has scale set to: mean = 0.5, sd = 0.0001"){
+  predictions %>% 
+    ggplot(aes(x = date)) +
+    geom_line(aes(y = median, color = "Median secondary estimate"), lwd=2) + 
+    geom_line(aes(y = mean, color = "Mean secondary estimate"), lty = 3, lwd =2)+
+    geom_line(aes(y = primary, color = "Primary data")) +
+    geom_line(aes(y = secondary, color = "Secondary data")) +
+    theme(legend.title = element_blank()) +
+    labs(y="Counts", x = "Date", title = plot_title)
+  
+}
