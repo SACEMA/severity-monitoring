33e4023cefc69f64e6433fbfe62faf7cbc5aa9c1
Bingham <jeremyb@sun.ac.za>
2022-03-07 15:03:05 +0200

    another simple data generation function

--- synth_data_functions.R
+++ synth_data_functions.R
@@ -12,6 +12,14 @@ generate_primary_linear <- function(length, base_inc, rate_of_increase){
 }
 
 generate_secondary_fixed_ratio <- function(primary, delay, ratio){
-  secondary <- c(rep(0, length(primary)))
-  secondary[delay+1:length(secondary)] = primary[delay+1:length(secondary)]*ratio
+  secondary <- rep(0, length(primary))
+  secondary[delay+1:length(secondary)] = primary[(delay+1:length(secondary))-delay]*ratio
+  return(secondary)
+}
+
+generate_secondary_two_ratio <- function(primary, delay, ratio1, ratio2, tchange_prim){
+  secondary <- rep(0, length(primary))
+  secondary[(1:tchange_prim)+delay] = primary[1:tchange_prim]*ratio1
+  secondary[((tchange_prim+1):(length(secondary)-delay))+delay] =  primary[((tchange_prim+1):(length(primary)-delay))]*ratio2
+  return(secondary)
 }
