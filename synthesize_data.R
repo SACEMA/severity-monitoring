31838304de03a869118bc466f817f170bef24a4c
Bingham <jeremyb@sun.ac.za>
2022-03-14 14:18:31 +0200

    minor adjustment to original example script + makefile targeting flat_constant.png in new directory structure

--- synthesize_data.R
+++ synthesize_data.R
@@ -27,4 +27,4 @@ reported_data <- tibble(date = ymd('2021-01-01') + 0:99,
 
 reported_data$secondary = replace_na(reported_data$secondary, 0)
 
-saveRDS(reported_data, './data/flat-ts-fixed-proportion.rds')
+saveRDS(reported_data, './synthetic/data/flat_constant.rds')
