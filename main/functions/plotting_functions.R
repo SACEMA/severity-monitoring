a8fd4704e6b4ae345cc24496aae9699c72914fbf
James Azam <jamesazam@sun.ac.za>
2022-04-04 11:40:06 +0200

    Updated the plotting script to plot the raw data

--- main/functions/plotting_functions.R
+++ main/functions/plotting_functions.R
@@ -1,27 +1,70 @@
-.args <- if(interactive()){c('./functions/plotting_functions.RData')}else{commandArgs(trailingOnly=TRUE)}
+.args <- if (interactive()) {
+  c("./functions/plotting_functions.RData")
+} else {
+  commandArgs(trailingOnly = TRUE)
+}
+
+predictions <- .args[1]
+data_raw <- .args[2]
+
 
 #' Plot basic output from estimate_secondary
 #'
 #' @param predictions As in, estimate_secondary()[['predictions']]
+#' @param data_raw  The raw data used for the predictions
 #' @param plot_title plot title
-#' @description Basic plot, showing primary and secondary input time series, 
+#'
+#' @description Basic plot, showing primary and secondary input time series,
 #' as well as median and mean time series of fit from estimate_secondary().
 #' @return
 #' @export
 #'
 #' @examples
 plot_est_sec_out <- function(predictions,
-                             plot_title = "obs_opts has scale set to: mean = 0.5, sd = 0.0001"){
-  predictions %>% 
-    ggplot(aes(x = date)) +
-    geom_line(aes(y = median, color = "Median secondary estimate"), lwd=2) + 
-    geom_line(aes(y = mean, color = "Mean secondary estimate"), lty = 3, lwd =2)+
-    geom_line(aes(y = primary, color = "Primary data")) +
-    geom_line(aes(y = secondary, color = "Secondary data")) +
+                             data_raw,
+                             plot_title = "obs_opts has scale set to: mean = 0.5, sd = 0.0001") {
+  ggplot(
+    data = predictions,
+    aes(x = date)
+  ) +
+    geom_line(aes(
+      y = median,
+      color = "Median secondary estimate"
+    ),
+    linetype = 3,
+    size = 2
+    ) +
+    geom_line(aes(
+      y = mean,
+      color = "Mean secondary estimate"
+    ),
+    linetype = 3,
+    size = 2
+    ) +
+    geom_line(aes(
+      y = primary,
+      color = "Primary data"
+    )) +
+    geom_line(aes(
+      y = secondary,
+      color = "Secondary data"
+    )) +
+    geom_line(
+      data = data_raw,
+      aes(
+        x = date,
+        y = primary_underlying,
+        color = "Underlying primary data"
+      )
+    ) +
+    geom_line(
+      x = date,
+      y = secondary_underlying,
+      color = "Underlying secondary data"
+    ) +
     theme(legend.title = element_blank()) +
-    labs(y="Counts", x = "Date", title = plot_title)
-  
+    labs(y = "Counts", x = "Date", title = plot_title)
 }
 
 
-save(list = ls(), file = tail(.args,1))
+save(list = ls(), file = tail(.args, 1))
