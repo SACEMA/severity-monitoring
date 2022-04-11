c34e11e3bae718b91c3726aaa9eeb808f9542e6a
James Azam <jamesazam@sun.ac.za>
2022-04-11 14:29:26 +0200

    Loaded more packages and added code to plot and combine the top and bottom panels

--- main/scripts/plot_sec_est.R
+++ main/scripts/plot_sec_est.R
@@ -1,10 +1,14 @@
 # plot output from estimate_secondary
 library(tidyverse)
+library(patchwork)
+library(ggthemes)
+library(scales)
+
 .args <- if (interactive()) c(
-  file.path('synthetic/outputs/full', 'flat_const_const_const.rds'), # input
-  file.path('functions','plotting_functions.RData'), # load functions
-  file.path('synthetic/data','flat_const_const_const.rds'), # input 2
-  file.path('synthetic/outputs/figures', 'flat_const_const_const.png') # output
+  file.path('synthetic/outputs/full', 'const_const_const_const.rds'), # input
+  file.path('main/data','plotting_functions.RData'), # load functions
+  file.path('synthetic/data','const_const_const_const.rds'), # input 2
+  file.path('synthetic/outputs/figures', 'const_const_const_const.png') # output
 ) else commandArgs(trailingOnly = TRUE)
 
 #load the data functions
@@ -27,6 +31,10 @@ dd <- left_join(dd_raw, dd_pred,
                        )
                 )
 
-fig <- plot_est_sec_out(dat = dd)
+fig_top_panel <- plot_est_sec_out(dat = dd)
 
-ggsave(tail(.args,1), plot = fig, width = 12, height = 8)
+fig_bottom_panel <- plot_ratios(dat = dd)
+
+fig_final <- fig_top_panel/fig_bottom_panel
+
+ggsave(tail(.args,1), plot = fig_final, width = 12, height = 8)
