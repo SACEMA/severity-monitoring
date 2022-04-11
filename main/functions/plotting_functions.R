8dfbcf7c97964f1cb8e008ef27e1366479dce77e
James Azam <jamesazam@sun.ac.za>
2022-04-11 14:30:02 +0200

    Reworded the top panel figure title

--- main/functions/plotting_functions.R
+++ main/functions/plotting_functions.R
@@ -17,7 +17,7 @@
 #'
 #' @examples
 plot_est_sec_out <- function(dat,
-                             plot_title = "obs_opts has prior of mean = 1 and sd = 1; default delay_opts()") {
+                             fig_title = "True, observed, and predicted outcomes") {
   ggplot(
     data = dat,
     aes(x = date)
