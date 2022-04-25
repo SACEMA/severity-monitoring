44b7c7d52a1e6c746e67a56471428a8ccb8ff206
jamesmbaazam <jamesazam@sun.ac.za>
2022-04-25 16:52:00 +0200

    Added scripts to define parameter ranges and generate param combinations for experiments

---
+++ synthetic/scripts/generate_param_combos.R
@@ -0,0 +1,42 @@
+.args <- if (interactive()) {
+    c("./synthetic/data/define_synth_params_mother_list.csv")
+} else {
+  commandArgs(trailingOnly = TRUE)
+}
+
+library(tidyverse)
+
+
+#Get param specifications
+mother_params_raw <- read_csv(.args[[1]])
+
+
+#Extract start date
+start_date <- as.Date((mother_params_raw %>% tail(1))$min)
+
+
+#Create column of vectors for grid
+mother_params <- mother_params_raw %>% 
+  filter(param != 'start_date') %>% 
+  mutate(min = as.numeric(min),
+         max = as.numeric(max)
+         ) %>% 
+  rowwise() %>% 
+  mutate(values = list(seq(from = min, to = max, by = n))
+         )
+  
+
+#Create a list of vectors per parameter
+mother_param_vec_list <- list()
+
+for (i in 1:nrow(mother_params)) {
+  mother_param_vec_list[[mother_params[[i, 'param']]]] <- seq(from = mother_params[[i, 'min']], 
+                                          to = mother_params[[i, 'max']],
+                                          length.out = mother_params[[i, 'n']]
+                                          )
+}
+
+#Generate the parameter combinations
+param_combos <- expand.grid(mother_param_vec_list)
+
+save(list = ls(), file = tail(.args, 1))
