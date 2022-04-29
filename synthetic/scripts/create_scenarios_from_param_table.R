02049df29b4df7f0e14086557b5499259bb6bfc3
Jeremy <jeremy@STIAS518336.stb.sun.ac.za>
2022-04-29 14:38:46 +0200

    create scenarios from param table

---
+++ synthetic/scripts/create_scenarios_from_param_table.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+
+.args <- if(interactive()){
+c("./synthetic/inputs/param_combos.rds",
+  "./synthetic/data/synth_data_functions.RData",
+  "./synthetic/data/flat_const_const_const.rds",
+  "a",
+  "a",
+  "a",
+  "a"
+  )
+}else{commandArgs(trailingOnly = TRUE)}
+
+param_table <- readRDS(.args[[1]])  # parameter names and values
+load(.args[[2]])  # function names with default parameters named,
+
+scenario_table <- param_table %>% row_wise() %>% create_scenario(.)
+
+
+
+# generically rename generation and observation functions according to the scenario description received via commandArgs()
+create_scenario <- function(param_row){
+	scenario <- (d1_fxn() %>%
+		 d2_fxn() %>%
+		 d3_fxn() %>%
+		 d4_fxn() %>%
+		 add_dates()
+		)
+	return(scenario)
+}
+
+d1_fxn <- get(d1_functions[[.args[4]]]) # dimension 1 functional form
+d2_fxn <- get(d2_functions[[.args[5]]]) # dimension 2 functional form
+d3_fxn <- get(d3_functions[[.args[6]]]) # dimension 3 functional form
+d4_fxn <- get(d4_functions[[.args[7]]]) # dimension 4 functional form
+
+
+dd <- d1_fxn() %>% d2_fxn() %>% d3_fxn() %>% d4_fxn() %>% add_dates()
+
+dd$primary = as.integer(dd$primary)
+dd$secondary = as.integer(dd$secondary)
+
+saveRDS(dd, .args[[3]])
+
