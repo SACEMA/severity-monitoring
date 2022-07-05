6791e0a767108b4121b211926291f0f7df40ad55
James Azam <jamesazam@sun.ac.za>
2022-07-05 15:36:34 +0200

    updated the scripts to create and plot repeated runs per scenario

--- synthetic/scripts/plot_scenario.R
+++ synthetic/scripts/plot_scenario.R
@@ -23,21 +23,14 @@ load(.args[1])
 
 scenario_desc <- read_json(.args[2])[["scen_desc"]]
 
-options(scipen = 9999)
+options(scipen = 9999) # remove scientific notation
 
-plot_data <- sim_results %>%
+plot_data <- scenario_data %>%
   mutate(name = str_replace(name, "_", " "))
 
-# ts_tmp <- ggplot(plot_data, aes(x = time, y = value, label = name))+
-#   geom_labelpath(size = 3.5) +
-#   labs(y = "Daily count", x = "Day",
-#        title = scenario_desc,
-#        color = "") +
-#   theme_minimal()
-
-plot_df <- plot_data %>% 
-  pivot_wider(names_from = name, values_from = value) %>%
-  janitor::clean_names()
+plot_labels <- scenario_data %>% 
+  group_by(name) %>% 
+  slice(1)
 
 ts_tmp_log <- ggplot(
   plot_data,
@@ -49,44 +42,26 @@ ts_tmp_log <- ggplot(
   )
 ) +
   geom_line() +
+  geom_labelline(
+    data = plot_labels,
+    aes(
+      label = name,
+      group = interaction(sim_id, name)
+    ),
+    hjust = 0,
+    size = 3.5,
+    linewidth = 0.45,
+    straight = TRUE
+  ) +
   scale_y_log10() +
   theme_minimal() +
-  theme(legend.position = 'bottom') +
-  labs(color = '', title = scenario_desc)
-  # geom_labelline(aes(
-  #   label = name,
-  #   group = sim_id
-  # ),
-  # hjust = 0.6,
-  # size = 3.5,
-  # linewidth = 0.45,
-  # straight = TRUE
-  # ) +
-  # scale_y_log10() +
-  # labs(
-#   y = "Daily count (log transformed)", x = "Day",
-#   color = ""
-# ) +
-# theme_minimal()
-  # geom_line() +
-  # geom_line(aes(
-  #   x = time,
-  #   y = latent_primary,
-  #   group = factor(sim_id),
-  #   color = 'latent_primary'
-  # )) +
-  # geom_line(aes(
-  #   x = time,
-  #   y = primary,
-  #   group = factor(sim_id)
-  # )) +
-  # geom_line(aes(
-  #   x = time,
-  #   y = secondary,
-  #   group = factor(sim_id)
-  # )) +
-  # scale_y_log10() +
-  # theme_minimal()
+  theme(legend.position = "none") +
+  labs(
+    x = "Day",
+    y = "Daily count (log transformed)",
+    color = "",
+    title = scenario_desc
+  )
 
 
 ggsave(
